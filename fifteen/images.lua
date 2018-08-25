77ddb35d53ad76130235450d1bc14cb5d99441cb
hahawoo <santos4124@gmail.com>
2018-08-25 15:54:52 +1000

    Update images.lua

--- fifteen/images.lua
+++ fifteen/images.lua
@@ -1,3 +1,22 @@
+if ONCE.start then
+    ONCE.start = false
+    local setColor = love.graphics.setColor
+    love.graphics.setColor = function(r, g, b, a)
+        local function c(a)
+            if type(a) == 'number' then
+                return a / 255
+            elseif type(a) == 'table' then
+                local t = {}
+                for i, v in ipairs(a) do
+                    table.insert(t, v / 255)
+                end
+                return t
+            end
+        end
+        setColor(c(r), c(g), c(b), c(a))
+    end
+end
+
 local pieceSize = 100
 local pieceDrawSize = pieceSize - 1
 local pieceColor = {100, 20, 150}
